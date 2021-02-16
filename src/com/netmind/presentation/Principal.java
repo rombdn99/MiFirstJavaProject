a31ac1bc74554223bfb8794a9dcfa18b7dc93490
raulosunamontes <raul41999@gmail.com>
2021-02-16 21:06:24 +0100

    Initial commit with calculator structure.

---
+++ src/com/netmind/presentation/Principal.java
@@ -0,0 +1,15 @@
+package com.netmind.presentation;
+
+import com.netmind.business.Calculator;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		Calculator calculadora = new Calculator();
+		System.out.println(calculadora.add(14, 15));
+		
+	}
+
+}
