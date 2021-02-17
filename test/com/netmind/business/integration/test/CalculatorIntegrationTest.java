2f641f4b4a13fa1b9ba4d9756ddf8622b4a04398
raulosunamontes <raul41999@gmail.com>
2021-02-17 19:26:32 +0100

    Calculator integration test implemented

--- test/com/netmind/business/integration/test/CalculatorIntegrationTest.java
+++ test/com/netmind/business/integration/test/CalculatorIntegrationTest.java
@@ -11,24 +11,25 @@ public class CalculatorIntegrationTest {
 	@Test
 	public void testAdd() {
 		Calculator calculator = new Calculator();
-		assertEquals(calculator.add(4, 2), 7);
-		
-		//fail("Not yet implemented");
+		assertEquals(calculator.add(4, 2), 6);
 	}
 
 	@Test
 	public void testSubstract() {
-		fail("Not yet implemented");
+		Calculator calculator = new Calculator();
+		assertEquals(calculator.substract(4, 2), 2);
 	}
 
 	@Test
 	public void testMultiply() {
-		fail("Not yet implemented");
+		Calculator calculator = new Calculator();
+		assertEquals(calculator.multiply(4, 2), 8);
 	}
 
 	@Test
 	public void testDivide() {
-		fail("Not yet implemented");
+		Calculator calculator = new Calculator();
+		assertEquals(calculator.divide(4, 2), 2);
 	}
 
 }
