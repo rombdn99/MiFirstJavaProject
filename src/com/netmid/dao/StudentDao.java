a769a4b25ab64f9c03695dc97f20fb998c65cebc
raulosunamontes <raul41999@gmail.com>
2021-02-20 13:15:10 +0100

    Three layer architecture implemented. closes #1

---
+++ src/com/netmid/dao/StudentDao.java
@@ -0,0 +1,11 @@
+package com.netmid.dao;
+
+import com.netmind.model.Student;
+
+public class StudentDao {
+
+	public boolean add(Student student) {
+		throw new UnsupportedOperationException(
+				"The file is not implemented yet");
+	}
+}
--- src/com/netmind/presentation/Principal.java
+++ src/com/netmind/presentation/Principal.java
@@ -1,13 +1,25 @@
 package com.netmind.presentation;
 
-//import com.netmind.business.Calculator;
-import com.netmind.business.SumaNumerosEnteros;
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		SumaNumerosEnteros suma= new SumaNumerosEnteros();
-		System.out.println(suma.sumaArray(suma.generacionArray()));
+
+		Scanner sc = new Scanner(System.in);
+
+		/*
+		 * Que opcion quiere seleccionar 1. Agregar un nuevo estudiante 2.
+		 * Calcular el estudiante con mayor edad 3. Calcular la media de edad de
+		 * todos los estudiantes 4. Salir
+		 * 
+		 * Si apreta un 1 tendra que ir a un menu que pregunte: nombre del
+		 * estudiante apellido del estudiante ...
+		 * 
+		 * Este estudiante lo guardaremos en uin Arraylist en la capa DAO
+		 * guardar se calculara la edad del alumno a partir de la fecha
+		 */
+
 	}
 
 }
