f4706872a9d77ae5b875856605e3ed381d7f27dc
raulosunamontes <raul41999@gmail.com>
2021-02-25 16:35:06 +0100

    Refactory Student close #6

--- src/com/netmind/model/Student.java
+++ src/com/netmind/model/Student.java
@@ -63,18 +63,24 @@ public class Student {
 
 	}
 
-	/*
-	 * @Override public String toString() { StringBuilder builder = new
-	 * StringBuilder(); builder.append("Student [idStudent=");
-	 * builder.append(idStudent); builder.append(", name=");
-	 * builder.append(name); builder.append(", surname=");
-	 * builder.append(surname); builder.append(", age="); builder.append(age);
-	 * builder.append(", dateOfBirth="); builder.append(dateOfBirth);
-	 * builder.append("]"); return builder.toString(); }
-	 */
-
 	@Override
 	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Student [idStudent=");
+		builder.append(idStudent);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", surname=");
+		builder.append(surname);
+		builder.append(", age=");
+		builder.append(age);
+		builder.append(", dateOfBirth=");
+		builder.append(dateOfBirth);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	public String toFileFormat() {
 		StringBuilder builder = new StringBuilder();
 		builder.append(idStudent);
 		builder.append(",");
