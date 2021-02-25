544a37523f0a770ccffffde8d100ea6936e002e3
raulosunamontes <raul41999@gmail.com>
2021-02-25 16:43:37 +0100

    Refactory StudentDao close #4

--- src/com/netmind/dao/StudentDao.java
+++ src/com/netmind/dao/StudentDao.java
@@ -7,47 +7,40 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import com.netmind.model.Student;
 
 public class StudentDao {
 
-	private static ArrayList<Student> studentList = null;
-	private static String fileLocation;
+	private static List<Student> studentList = null;
 	private static File file;
+
 	static {
 		studentList = new ArrayList<Student>(); // Así se hace correctamente
-		fileLocation = "prueba.txt";
 		file = new File("StudentList.txt");
 	}
 
 	public boolean add(Student student) {
-		try {
+		studentList.add(student);
+		return true;
+	}
 
+	public boolean addToTxt(Student student) {
+		try {
 			if (!file.exists()) {
 				file.createNewFile();
-				file.deleteOnExit();
 			}
 			FileWriter writer = new FileWriter(file, true);
 			BufferedWriter bufferedWriter = new BufferedWriter(writer);
-			student.setIdStudent(studentList.size() + 1);
-			studentList.add(student);
-			bufferedWriter.write(student.toString());
+			bufferedWriter.write(student.toFileFormat());
 			bufferedWriter.newLine();
 			bufferedWriter.close();
 			return true;
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return false;
 		}
-
-		// Crearemos un unico ArrayList y agregamos el estudent en el arraylist
-		/*
-		 * Se agrega el student a un arraylist si no se produce ningun error se
-		 * devuelve true
-		 */
-
 	}
 
 	public static void readList() {
@@ -57,25 +50,27 @@ public class StudentDao {
 		}
 	}
 
-	public static void readFile() {
+	public static String readFile() {
 		try {
 			FileReader reader = new FileReader(file);
 			BufferedReader bufferedReader = new BufferedReader(reader);
 			String line;
+			StringBuilder builder = new StringBuilder();
 			while ((line = bufferedReader.readLine()) != null) {
-				System.out.println(line);
+				builder.append(line);
 			}
 			reader.close();
-
+			return builder.toString();
 		} catch (IOException e) {
 			e.printStackTrace();
+			return "";
 		}
 
 	}
 
 	public static void writeFile() {
 		try {
-			FileWriter writer = new FileWriter("MyFile.txt", true);
+			FileWriter writer = new FileWriter(file, true);
 			BufferedWriter bufferedWriter = new BufferedWriter(writer);
 
 			bufferedWriter.write("Hello World");
