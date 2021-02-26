ec236a0627ecb5189ba2d92e772ceedd6fff782e
raulosunamontes <raul41999@gmail.com>
2021-02-26 11:00:58 +0100

    Change option int to enum. close #9

---
+++ src/com/netmind/model/EnumStudent.java
@@ -0,0 +1,5 @@
+package com.netmind.model;
+
+public enum EnumStudent {
+	EXIT, ADD_STUDENT, OLDEST_STUDENT, READ_STUDENTFILE
+}
--- src/com/netmind/presentation/Principal.java
+++ src/com/netmind/presentation/Principal.java
@@ -3,6 +3,7 @@ package com.netmind.presentation;
 import java.util.Scanner;
 
 import com.netmind.business.StudentBl;
+import com.netmind.model.EnumStudent;
 
 public class Principal {
 
@@ -11,20 +12,24 @@ public class Principal {
 		Scanner sc = new Scanner(System.in);
 		StudentBl studentBl = new StudentBl();
 		Menu menus = new Menu();
+		EnumStudent option;
 		boolean exit = true;
 		do {
-
-			switch (menus.menuPrincipal(sc)) {
-			case 1:
+			option = EnumStudent.values()[menus.menuPrincipal(sc)];
+			switch (option) {
+			case ADD_STUDENT:
 				menus.menuAddStudent(sc);
 				break;
-			case 2:
+			case OLDEST_STUDENT:
 				System.out.print("La edad maxima de los estudiantes es ");
 				System.out.println(studentBl.maximunStudentAge());
 				break;
+			case READ_STUDENTFILE:
+				System.out.println(studentBl.readFile());
+				break;
 			default:
 				exit = false;
-
+				break;
 			}
 
 		} while (exit);
