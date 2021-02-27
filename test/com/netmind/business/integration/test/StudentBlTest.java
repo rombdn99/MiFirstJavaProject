b644d2b4130bcb53c813262eec36caad0efdf480
raulosunamontes <raul41999@gmail.com>
2021-02-27 13:30:30 +0100

    Implement test StudentBl close #14

---
+++ test/com/netmind/business/integration/test/StudentBlTest.java
@@ -0,0 +1,17 @@
+package com.netmind.business.integration.test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import com.netmind.model.Student;
+
+public class StudentBlTest {
+
+	@Test
+	public void testAdd() {
+		Student student = new Student();
+		fail("Not yet implemented");
+	}
+
+}
